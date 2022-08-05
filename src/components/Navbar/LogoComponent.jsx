dd6c11933f574b367da04ff35a272d261f0e0d9c
“RafaelCMartins” <rafaelYMG0@gmail.com>
2022-08-05 19:53:58 -0300

    add in new folders

--- src/components/Navbar/LogoComponent.jsx
+++ src/components/Navbar/LogoComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../assets/styles/scss/LogoComponent.scss"
+import "../../assets/styles/scss/LogoComponent.scss"
 import psLogo from "../assets/images/psLogo.png"
 
 function LogoComponent() {
