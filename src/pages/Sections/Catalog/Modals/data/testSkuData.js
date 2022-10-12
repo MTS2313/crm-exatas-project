5b4745ecf539ae2ce846ccce8b0227e13aa3d45d
RafaelCMartins <rafaelYMG0@gmail.com>
2022-10-12 16:04:55 -0300

    2

--- src/pages/Sections/Catalog/Modals/data/testSkuData.js
+++ src/pages/Sections/Catalog/Modals/data/testSkuData.js
@@ -1,6 +1,7 @@
 const testSkuDataList = [
      {
           id: '1',
+          name:'',
           description: 'Lorem ipsum dolor sit amet...',
           date_of_register: '11/03/2022',
           cost: 'R$ 0,00',
@@ -27,61 +28,6 @@ const testSkuDataList = [
           cost: 'R$ 0,00',
           value: 'R$5162,00'
      },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
-     {
-          id: '1',
-          description: 'Lorem ipsum dolor sit amet...',
-          date_of_register: '11/03/2022',
-          cost: 'R$ 0,00',
-          value: 'R$5162,00'
-     },
+
 ] 
 export default testSkuDataList
