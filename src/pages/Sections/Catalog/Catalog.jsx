506d7bafb24ac76aea7f3a4e96f6ddeb1d72d73c
RafaelCMartins <rafaelYMG0@gmail.com>
2022-10-12 10:58:47 -0300

    add material design

--- src/pages/Sections/Catalog/Catalog.jsx
+++ src/pages/Sections/Catalog/Catalog.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../../components/GlobalComponents/Header/Header";
-import TableLayout from "./Table/Table";
+import TableLayout from "./Table/TableProduct";
 import testSkuDataList from "./Modals/data/testSkuData";
 import schema from "../../../assets/data/tables/product.schema";
 import "./Modals/style/Catalog.scss"
